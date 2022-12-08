4d3185ae6e553da0658d494682abca737ff7b337
Victor Rodriguez <victor.rodriguez.guriz@gmail.com>
2022-12-08 12:03:57 -0500

    Change styles to match Windster theme

--- app/views/dashboard/sidebar/bar.rb
+++ app/views/dashboard/sidebar/bar.rb
@@ -1,16 +1,26 @@
 module Views::Dashboard::Sidebar
   class Bar < Views::ApplicationView
     def template
-      aside(class: "w-64") do
-        div(class: "overflow-y-auto py-4 px-3 bg-gray-50 rounded-md") do
-          ul(class: "space-y-2") do
-            render(Item.new(text: "Teams", href: dashboard_teams_path))
-            7.times do |i|
-              render(Item.new(text: "Sidebar Item #{i}", href: "#"))
+      aside(
+        :id => "sidebar",
+        :class => "fixed hidden z-20 h-full top-0 left-0 pt-16 flex lg:flex flex-shrink-0 flex-col w-64 transition-width duration-75",
+        :"aria-label" => "Sidebar"
+      ) do
+        div(class: "relative flex-1 flex flex-col min-h-0 border-r border-gray-200 bg-white pt-0") do
+          div(class: "flex-1 flex flex-col pt-5 pb-4 overflow-y-auto") do
+            div(class: "flex-1 px-3 bg-white divide-y space-y-1") do
+              ul(class: "space-y-2 pb-2") do
+                render(Item.new(text: "Teams", href: dashboard_teams_path))
+                7.times do |i|
+                  render(Item.new(text: "Sidebar Item #{i}", href: "#"))
+                end
+              end
             end
           end
         end
       end
+
+      div(class: "bg-gray-900 opacity-50 hidden fixed inset-0 z-10", id: "sidebarBackdrop")
     end
   end
 end
--- app/views/pages/dashboard.rb
+++ app/views/pages/dashboard.rb
@@ -4,7 +4,56 @@ module Views
 
       def template
         render(Views::Dashboard::Layout.new(title: "Dashboard")) do
-          h1 { "Placeholder content " }
+          # cards
+          div(class: "flex gap-10 mt-7 justify-center") do
+            # <= CARD BEGIN
+            div(class: "flex flex-col w-52 h-40 rounded-md shadow-md justify-between px-3 pb-2 pt-3") do
+              div(class: "flex flex-row justify-between") do
+                div(class: "flex text-xl w-10") { "🛒" }
+                div(class: "flex w-13 justify-center text-xs items-center rounded-full bg-emerald-300 text-white px-1.5 py-0.5") { "+20%" }
+              end
+
+              div(class: "") do
+                h3(class: "font-bold text-3xl") { "2,400" }
+                p(class: "text-base text-gray-500") { "items sales" }
+              end
+            end
+            # CARD END =>
+            # <= CARD BEGIN
+            div(class: "flex flex-col w-52 h-40 rounded-md shadow-md justify-between px-3 pb-2 pt-3") do
+              div(class: "flex flex-row justify-between") do
+                div(class: "flex text-xl w-10") do
+                  "🏪"
+                end
+
+                div(class: "flex w-13 justify-center text-xs items-center rounded-full bg-rose-500 text-white px-1.5 py-0.5") do
+                  "-12%"
+                end
+              end
+
+              div(class: "") do
+                h3(class: "font-bold text-3xl") { "400" }
+                p(class: "text-base text-gray-500") { "orders placed" }
+              end
+            end
+            # CARD END =>
+            # <= CARD BEGIN
+            div(class: "flex flex-col w-52 h-40 rounded-md shadow-md justify-between px-3 pb-2 pt-3") do
+              div(class: "flex flex-row justify-between") do
+                div(class: "flex text-xl w-10") { "🛒" }
+
+                div(class: "flex w-13 justify-center text-xs items-center rounded-full bg-amber-300 text-white px-1.5 py-0.5") do
+                  "~0%"
+                end
+              end
+
+              div(class: "") do
+                h3(class: "font-bold text-3xl") { "29000" }
+                p(class: "text-base text-gray-500") { "new registered users" }
+              end
+            end
+            # CARD END =>
+          end
         end
       end
     end
