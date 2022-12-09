bd97c0587fa60c3a71625c4abc7adcbb9fe51773
Victor Rodriguez <victor.rodriguez.guriz@gmail.com>
2022-12-08 19:18:51 -0500

    Move SVGs to module

--- app/views/svg.rb
+++ app/views/svg.rb
@@ -30,5 +30,38 @@ module Views
         )
       end
     end
+
+    def magnifying_glass_icon
+      svg(
+        class: "w-5 h-5 text-gray-500",
+        fill: "currentColor",
+        viewBox: "0 0 20 20",
+        xmlns: "http://www.w3.org/2000/svg"
+      ) do
+        path(
+          :"fill-rule" => "evenodd",
+          :d => "M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z",
+          :"clip-rule" => "evenodd"
+        )
+      end
+    end
+
+    def home_icon
+      svg(class: "w-5 h-5 mr-2.5", fill: "currentColor", viewBox: "0 0 20 20", xmlns: "http://www.w3.org/2000/svg") do
+        path(
+          d: "M10.707 2.293a1 1 0 00-1.414 0l-7 7a1 1 0 001.414 1.414L4 10.414V17a1 1 0 001 1h2a1 1 0 001-1v-2a1 1 0 011-1h2a1 1 0 011 1v2a1 1 0 001 1h2a1 1 0 001-1v-6.586l.293.293a1 1 0 001.414-1.414l-7-7z"
+        )
+      end
+    end
+
+    def arrow_icon
+      svg(class: "w-6 h-6 text-gray-400", fill: "currentColor", viewBox: "0 0 20 20", xmlns: "http://www.w3.org/2000/svg") do
+        path(
+          :"fill-rule" => "evenodd",
+          :d => "M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z",
+          :"clip-rule" => "evenodd"
+        )
+      end
+    end
   end
 end
