be07f097dde9a637fea519eaa46ff6eba0bc2187
Gerard Altamirano <hello@itsgerard.com>
2022-12-09 15:08:25 -0500

    Extend Nav::Item component
    - Add a way to delegate content of element via a block

--- app/views/dashboard/nav/item.rb
+++ app/views/dashboard/nav/item.rb
@@ -1,17 +1,23 @@
 module Views::Dashboard::Nav
   class Item < Views::ApplicationView
-    def initialize(href:, text:)
-      @text = text
+    def initialize(href: "#", text: "", class_name: nil)
       @href = href
+      @text = text
+      @class_name = class_name
     end
 
-    def template
-      li { a(href: @href, class: link_style) { @text } }
+    def template(&custom_content)
+      li(class: @class_name) do
+        return yield_content {
+          custom_content.call(element_style)
+        } if custom_content.present?
+        
+        a(href: @href, class: element_style) { @text }
+      end
     end
 
     private
-
-    def link_style
+    def element_style
       <<-STYLES.squish
         text-base text-gray-900 font-normal rounded-lg flex items-center p-2 hover:bg-gray-100 group
       STYLES
