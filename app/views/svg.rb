8408df189538e149db5c73dd79bffd77167710c9
Gerard Altamirano <hello@itsgerard.com>
2022-12-09 14:29:58 -0500

    Add user and notifications svg icons

--- app/views/svg.rb
+++ app/views/svg.rb
@@ -1,7 +1,7 @@
 module Views
   module SVG
     def plus_icon
-      svg(class: "-ml-1 mr-2 h-6 w-6", fill: "currentColor", viewBox: "0 0 20 20", xmlns: "http://www.w3.org/2000/svg") do
+      svg(class: "-ml-1 mr-2 h-6 w-6", fill: "currentColor", viewBox: "0 0 20 20", xmlns: xmlns) do
         path(
           :"fill-rule" => "evenodd",
           :d => "M10 5a1 1 0 011 1v3h3a1 1 0 110 2h-3v3a1 1 0 11-2 0v-3H6a1 1 0 110-2h3V6a1 1 0 011-1z",
@@ -11,7 +11,7 @@ module Views
     end
 
     def trash_icon
-      svg(class: "mr-2 h-5 w-5", fill: "currentColor", viewBox: "0 0 20 20", xmlns: "http://www.w3.org/2000/svg") do
+      svg(class: "mr-2 h-5 w-5", fill: "currentColor", viewBox: "0 0 20 20", xmlns: xmlns) do
         path(
           :"fill-rule" => "evenodd",
           :d => "M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z",
@@ -21,7 +21,7 @@ module Views
     end
 
     def pen_to_square_icon
-      svg(class: "mr-2 h-5 w-5", fill: "currentColor", viewBox: "0 0 20 20", xmlns: "http://www.w3.org/2000/svg") do
+      svg(class: "mr-2 h-5 w-5", fill: "currentColor", viewBox: "0 0 20 20", xmlns: xmlns) do
         path(d: "M17.414 2.586a2 2 0 00-2.828 0L7 10.172V13h2.828l7.586-7.586a2 2 0 000-2.828z")
         path(
           :"fill-rule" => "evenodd",
@@ -36,7 +36,7 @@ module Views
         class: "w-5 h-5 text-gray-500",
         fill: "currentColor",
         viewBox: "0 0 20 20",
-        xmlns: "http://www.w3.org/2000/svg"
+        xmlns: xmlns
       ) do
         path(
           :"fill-rule" => "evenodd",
@@ -47,7 +47,7 @@ module Views
     end
 
     def home_icon
-      svg(class: "w-5 h-5 mr-2.5", fill: "currentColor", viewBox: "0 0 20 20", xmlns: "http://www.w3.org/2000/svg") do
+      svg(class: "w-5 h-5 mr-2.5", fill: "currentColor", viewBox: "0 0 20 20", xmlns: xmlns) do
         path(
           d: "M10.707 2.293a1 1 0 00-1.414 0l-7 7a1 1 0 001.414 1.414L4 10.414V17a1 1 0 001 1h2a1 1 0 001-1v-2a1 1 0 011-1h2a1 1 0 011 1v2a1 1 0 001 1h2a1 1 0 001-1v-6.586l.293.293a1 1 0 001.414-1.414l-7-7z"
         )
@@ -55,13 +55,41 @@ module Views
     end
 
     def arrow_icon
-      svg(class: "w-6 h-6 text-gray-400", fill: "currentColor", viewBox: "0 0 20 20", xmlns: "http://www.w3.org/2000/svg") do
+      svg(class: "w-6 h-6 text-gray-400", fill: "currentColor", viewBox: "0 0 20 20", xmlns: xmlns) do
         path(
           :"fill-rule" => "evenodd",
           :d => "M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z",
           :"clip-rule" => "evenodd"
         )
       end
+    end
+    
+    def notification_icon
+      svg(class: "w-5 h-5 text-gray-500", fill: "currentColor", viewBox: "0 0 20 20", xmlns: xmlns) do
+        path(
+          :"fill-rule" => "evenodd",
+          :d => "M10 2a6 6 0 00-6 6v3.586l-.707.707A1 1 0 004 14h12a1 1 0 00.707-1.707L16 11.586V8a6 6 0 00-6-6zM10 18a3 3 0 01-3-3h6a3 3 0 01-3 3z",
+          :"clip-rule" => "evenodd"
+        )
+      end
+    end
+    
+    def user_icon
+      svg(class: "w-5 h-5 text-gray-500", stroke_width: "1.5", fill: "currentColor", viewBox: "0 0 24 24", xmlns: xmlns) do
+        path(
+          :"fill-rule" => "evenodd",
+          :d => "M15.75 6a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0zM4.501 20.118a7.5 7.5 0 0114.998 0A17.933 17.933 0 0112 21.75c-2.676 0-5.216-.584-7.499-1.632z",
+          :"clip-rule" => "evenodd",
+          :"stroke-linecap" => "round",
+          :"stroke-linejoin" => "round",
+        )
+      end
+    end
+    
+    private 
+    
+    def xmlns
+      "http://www.w3.org/2000/svg"
     end
   end
 end
