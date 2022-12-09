4e49d943733e67cb5e9fc5a50a0165c971e4f8e6
Victor Rodriguez <victor.rodriguez.guriz@gmail.com>
2022-12-09 11:31:25 -0500

    Add livereload to views

--- app/views/layouts/application.rb
+++ app/views/layouts/application.rb
@@ -7,19 +7,21 @@ module Views
         @title = title
       end
 
-      def template(&)
+      def template(&content)
+        doctype
         html do
           head do
-            meta charset: "utf-8"
+            meta(charset: "utf-8")
             csp_meta_tag
             csrf_meta_tags
-            meta name: "viewport", content: "width=device-width,initial-scale=1"
+            meta(name: "viewport", content: "width=device-width,initial-scale=1")
             title { @title }
-            stylesheet_link_tag "application"
-            javascript_include_tag "application"
-          end
+            stylesheet_link_tag("application", :"data-turbo-track" => "reload")
+            javascript_include_tag("application", :"data-turbo-track" => "reload", :defer => true)
+            javascript_include_tag("hotwire-livereload", defer: true) if Rails.env.development?
 
-          body(class: "bg-gray-50", &)
+            body(class: "bg-gray-50", &content)
+          end
         end
       end
     end
