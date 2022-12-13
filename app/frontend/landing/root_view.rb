f7f094a97f246b9fe6367ef9c19fbf0fd3f9e717
Victor Rodriguez <victor.rodriguez.guriz@gmail.com>
2022-12-13 09:17:44 -0500

    Fix layout on landing

--- app/frontend/landing/root_view.rb
+++ app/frontend/landing/root_view.rb
@@ -1,7 +1,7 @@
 module Landing
   class RootView < Phlex::HTML
     def template
-      render(Views::Layouts::Application.new(title: "Landing")) do
+      render(Shared::Layouts::BaseLayout.new(title: "Landing")) do
         h1(class: "text-5xl font-bold text-center") do
           "LANDING PAGE IN PROGRESS!"
         end
