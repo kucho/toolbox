5da770b1bf51444620210d7a2f75d004973dd9dd
Gerard Altamirano <hello@itsgerard.com>
2022-12-14 04:22:50 -0500

    Add event subscriptions to Organization model

--- app/models/organization.rb
+++ app/models/organization.rb
@@ -4,6 +4,18 @@ class Organization < ApplicationRecord
 
   before_create :set_slug_and_subdomain
 
+  class Configuration
+    def call
+      event_store.subscribe(Organizations::Handlers::CreateOrganizationJob, to: [Organizations::Events::OrganizationCreated])
+    end
+
+    private
+
+    def event_store
+      Rails.configuration.event_store
+    end
+  end
+
   private
 
   def set_slug_and_subdomain
